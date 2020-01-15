3e3a57074d1f88ac08747b1345b4a0e63e9c631f
Jud Taylor <jud.taylor@protonmail.com>
2020-01-15 10:37:42 -0500

    correct order data are returned in.

--- lambdata_gptix/df_utils.py
+++ lambdata_gptix/df_utils.py
@@ -2,7 +2,6 @@
 Utilities fuctions for working with DataFrames.
 """
 
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
@@ -20,18 +19,16 @@ Split is currently hard-coded as 25% train, 50% validate, 25% test.
         # Split inputs, X_val and y_val are final.
         X_temp, X_val, y_temp, y_val = train_test_split(X, y, train_size=0.5)
         # Split X_temp and y_temp again.
-        X_train, X_test, y_train, y_test = train_test_split(X_temp, y_temp, train_size=0.5)
+        X_train, X_test, y_train, y_test =
+        train_test_split(X_temp, y_temp, train_size=0.5)
 
-        return (X_train, X_test, X_val, y_val, y_train, y_test)
+        return X_train, X_val, X_test, y_train, y_val, y_test
 
 
 def add_list_to_frame(dataframe, lst, colname):
         """
 Add a list as a new column in a dataframe, with specified column name.
 """
-# TODO - validate lengths of dataframe and list. Maybe error.        
-        dataframe.colname = lst
+# TODO - validate lengths of dataframe and list. Maybe error.
+        dataframe[colname] = lst
         return dataframe
-        
-        
-        
