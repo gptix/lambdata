34cd3c87e5c23c509194660e2c47eeb57b865a87
Jud Taylor <jud.taylor@protonmail.com>
2020-01-16 17:23:26 -0500

    Push update of README to test.pypi

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
     setuptools.setup(
         name="lambdata-gptix",
-        version="0.1.2",
+        version="0.1.3",
         author="gptix",
         description="a collection of data science helper functions",
         long_description=LONG_DESCRIPTION,
